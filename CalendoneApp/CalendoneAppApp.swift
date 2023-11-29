046e6a4dc7c511f11d14970555908e3f4a01a799
Luca Blanco Marchese <blancomarchese.bs@gmail.com>
2023-11-29 23:03:16 +0100

    Initial Commit

---
+++ CalendoneApp/CalendoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendoneAppApp.swift
+//  CalendoneApp
+//
+//  Created by Luca Blanco Marchese on 29.11.23.
+//
+
+import SwiftUI
+
+@main
+struct CalendoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
